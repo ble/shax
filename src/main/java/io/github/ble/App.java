b73d4b867377fd1e49e99d303be4dad82a2e5edc
Ben Ellis <benjaminster@gmail.com>
2014-12-13 23:58:07 -0500

    get crazy with the cheez-whiz

--- src/main/java/io/github/ble/App.java
+++ src/main/java/io/github/ble/App.java
@@ -1,33 +1,39 @@
 package io.github.ble;
 
 //Types that we might throw
-import org.xml.sax.SAXException;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException; 
+import io.github.ble.shax.util.DFSElementIterable;
+import io.github.ble.shax.util.NodeListIterable;
 
 import java.io.File;
-
+import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
+
+
 
 //DOM-style XML parsing types
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
 
-import io.github.ble.shax.util.NodeListIterable;
-import io.github.ble.shax.util.DFSElementIterable;
-
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.base.Joiner;
-import com.google.common.base.Function;
 
 /**
  * Hello world!
@@ -36,11 +42,98 @@ import com.google.common.base.Function;
 public class App 
 {
 	public static void main( String[] args )
-		throws SAXException, ParserConfigurationException, IOException
+		throws Exception
 	{
 		for(String arg: args) {
-			processFile(new File(arg));
+			//processFile(new File(arg));
+			countLines(new File(arg));
 		}
+	}
+
+	static void countLines(File f) throws Exception {
+		DocumentBuilder builder = instantiateBuilder();
+		Document doc = builder.parse(f);
+		Element root = doc.getDocumentElement();
+		XPathFactory xpf = XPathFactory.newInstance();
+		XPath xpi = xpf.newXPath();
+		XPathExpression xpe = xpi.compile("//LINE");
+
+		NodeList result = (NodeList) xpe.evaluate(root, XPathConstants.NODESET);
+
+		Random r = new Random();
+		for(int ix = 0; ix < 100; ix++) {
+			int selected = r.nextInt(result.getLength());
+			describeLine(result.item(selected));
+		}
+
+
+	}
+
+	static final XPathFactory xpf = XPathFactory.newInstance();
+	static final XPath xpi = xpf.newXPath();
+
+	private static XPathExpression ForceCompile(XPath xp, String expText) {
+		try {
+			return xp.compile(expText);
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	static final XPathExpression speakerOf = ForceCompile(xpi,"./preceding-sibling::SPEAKER[text()]");
+	static final XPathExpression lineNumberOf = ForceCompile(xpi, "count(./preceding::LINE)");
+	static final XPathExpression textOf = ForceCompile(xpi, "self::node()[text()]");
+	static final XPathExpression sceneOf = ForceCompile(xpi, "count(./ancestor::SCENE/preceding-sibling::SCENE) + 1");
+	static final XPathExpression actOf = ForceCompile(xpi, "count(./ancestor::ACT/preceding-sibling::ACT) + 1");
+	static final XPathExpression speechInScene = ForceCompile(xpi, "count(./ancestor::SPEECH/preceding-sibling::SPEECH) + 1");
+	static final XPathExpression lineInSpeech = ForceCompile(xpi, "count(./preceding-sibling::LINE) + 1");
+	static final XPathExpression linesInSpeech = ForceCompile(xpi, "count(./ancestor::SPEECH/child::LINE)");
+
+	static int evalInt(Node n, XPathExpression xpe) throws XPathExpressionException {
+		return ((Double) xpe.evaluate(n, XPathConstants.NUMBER)).intValue();
+	}
+
+	static String evalString(Node n, XPathExpression xpe) throws XPathExpressionException {
+		return ((String) xpe.evaluate(n, XPathConstants.STRING));
+	}
+
+	static void describeLine(Node n) throws Exception {
+
+		String speaker = (String) speakerOf.evaluate(n, XPathConstants.STRING);
+		/*int lineNumber; {
+			Double lineNum = (Double) lineNumberOf.evaluate(n, XPathConstants.NUMBER);
+			lineNumber = lineNum.intValue();
+		}*/
+		String lineText = evalString(n, textOf);
+		int actNumber = evalInt(n, actOf);
+		int sceneNumber = evalInt(n, sceneOf);
+		int speechNumber = evalInt(n, speechInScene);
+		int lineNumber = evalInt(n, lineInSpeech);
+		int totalLinesInSpeech = evalInt(n, linesInSpeech);
+
+		System.out.println(
+				String.format(
+						"Act %d scene %d, speech %d, line %d of %d\n\t %s: \"%s\"",
+						actNumber,
+						sceneNumber,
+						speechNumber,
+						lineNumber,
+						totalLinesInSpeech,
+						speaker, lineText));
+
+
+	}
+
+	static String extractChildText(Node n) throws Exception {
+		StringBuilder sb = new StringBuilder();
+
+		for(Node c: NodeListIterable.wrap(n.getChildNodes())) {
+			if(c.getNodeType() == Node.TEXT_NODE) {
+				sb.append( ((Text)c).getData());
+			}
+		}
+
+		return sb.toString();
 	}
 
 	static void processFile(File f) throws
